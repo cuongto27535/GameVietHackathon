3172a4ec829f3cb301c87e7f24ab665f6022f183
cuongto27535 <cuongtlph27535@fpt.edu.vn>
2023-09-03 15:18:44 +0700

    add

--- Assets/Vachamphong1.cs
+++ Assets/Vachamphong1.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vachamphong : MonoBehaviour
+public class Vachamphong1 : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject enemy;
+    public GameObject enemy1;
     void Start()
     {
-        enemy.SetActive(false);
+        enemy1.SetActive(false);
     }
 
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-         if(collision.gameObject.CompareTag("Player")){
-            enemy.SetActive(true);
+        if(collision.gameObject.CompareTag("Player")){
+            enemy1.SetActive(true);
         }
     }
 }
--- Assets/Vachamphong4.cs
+++ Assets/Vachamphong4.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vachamphong : MonoBehaviour
+public class Vachamphong4 : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject enemy;
+    public GameObject enemy4;
     void Start()
     {
-        enemy.SetActive(false);
+        enemy4.SetActive(false);
     }
 
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-         if(collision.gameObject.CompareTag("Player")){
-            enemy.SetActive(true);
+        if(collision.gameObject.CompareTag("Player")){
+            enemy4.SetActive(true);
         }
+        
     }
 }
